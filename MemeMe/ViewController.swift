5d50415926180018bba40c1ac1d007f6d0f84a97
Shir Bar Lev <shirblc@gmail.com>
2021-01-04 12:04:31 +0000

    Added the ability to select photos from Photos.

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -7,11 +7,72 @@
 
 import UIKit
 import AVFoundation
+import PhotosUI
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, PHPickerViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    // MARK: Meme from Photos methods
+    // checkPhotosPermission
+    // Checks that the app has permission to access the user's Photos library.
+    @IBAction func checkPhotosPermission(_ sender: Any) {
+        switch PHPhotoLibrary.authorizationStatus(for: .readWrite) {
+            // If the app has permission for accessing Photos, open the picker
+            case .authorized:
+                openPhotos()
+            // If the user hasn't given/denied permission, ask for permission. If Photos access is granted, open Photos
+            // for the user to select a photo. If the user selected limited access, show them the limited picker in order
+            // to select photos. Otherwise alert the user they've denied permission.
+            case .notDetermined:
+                PHPhotoLibrary.requestAuthorization(for: .readWrite, handler: { status in
+                    if status == .authorized {
+                        self.openPhotos()
+                    }
+                    else if status == .limited {
+                        let selectPhotos = PHPhotoLibrary.shared()
+                        selectPhotos.presentLimitedLibraryPicker(from: self)
+                    }
+                    else {
+                        print("Photos access has been denied. Please enable Photos access in Settings -> Privacy -> Photos.")
+                    }
+                })
+            // If the user denied Photos access, alert the user they've denied permission.
+            case .denied:
+                print("Photos access has been denied. Please enable Photos access in Settings -> Privacy -> Photos.")
+            // If the app has limited photo access, show the library.
+            // TODO: Display only selected assets
+            case .limited:
+                openPhotos()
+            // If Photos access has been restricted, alert the user.
+            case .restricted:
+                print("Photos access has been restricted. Please enable Photos access in Settings -> Privacy -> Photos.")
+            // For any other authorization status, alert the user that something went wrong.
+            @unknown default:
+                print("An unknown error occurred.")
+        }
+    }
+    
+    // openPhotos
+    // Presents the Photos library for the user to select a photo.
+    func openPhotos() {
+        // Configuration for the picker
+        var pickerConfig = PHPickerConfiguration()
+        pickerConfig.filter = .images
+        pickerConfig.selectionLimit = 1
+        // Create and launch photo picker
+        let photoPicker = PHPickerViewController(configuration: pickerConfig)
+        photoPicker.delegate = self
+        present(photoPicker, animated: true, completion: nil)
+    }
+    
+    // picker
+    // Protocol: PHPickerViewControllerDelegate
+    // Responsible for handling the photo the user picked.
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        print("picked")
     }
 
     // MARK: Meme from camera methods
