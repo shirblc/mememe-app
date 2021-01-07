3e71c82e4b98c4f1b757578bd047defc39b2eb38
Shir Bar Lev <shirblc@gmail.com>
2021-01-07 19:27:45 +0000

    Added SentMemes ViewControllers classes.

---
+++ MemeMe/SentMemesCollectionViewController.swift
@@ -0,0 +1,51 @@
+//
+//  SentMemesCollectionViewController.swift
+//  MemeMe
+//
+//  Created by Shir Bar Lev on 07/01/2021.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class SentMemesCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(systemItem: .add)
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(systemItem: .edit)
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+}
