b8eec18a0422ded6915a82a54bc3a01a643c0a26
Shir Bar Lev <shirblc@gmail.com>
2021-01-09 13:22:14 +0000

    Added an edit button to MemeDetail view.

--- MemeMe/MemeDetailViewController.swift
+++ MemeMe/MemeDetailViewController.swift
@@ -20,6 +20,8 @@ class MemeDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editMeme(_:)))
 
         if let memeIndex = memeIndex, memeIndex < appDelegate.memes.count {
             self.meme = appDelegate.memes[memeIndex]
@@ -27,12 +29,27 @@ class MemeDetailViewController: UIViewController {
         }
     }
     
-    // MARK: UI
+    // MARK: UI & Navigation
     
     // shareMeme
     // Shares the current edit as an image.
     @IBAction func shareMeme(_ sender: Any) {
         let shareViewController = UIActivityViewController(activityItems: [ self.meme as Any ], applicationActivities: nil)
         present(shareViewController, animated: true, completion: nil)
+    }
+    
+    // editMeme
+    // Trigger a transition to meme edit screen.
+    @objc func editMeme(_ sender: Any) {
+        performSegue(withIdentifier: "editMemeSegue", sender: sender)
+    }
+    
+    // Prepare for navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // If it's due to the edit button being pressed, set the Edit ViewController's memeIndex to the current meme's index.
+        if segue.identifier == "editMemeSegue" {
+            let editMemeVC = segue.destination as! MemeEditViewController
+            editMemeVC.memeIndex = self.memeIndex
+        }
     }
 }
